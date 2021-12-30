819def6192d4403a602cbf6cf26047b9bf5ab70c
unknown <ruben.fabio2016@gmail.com>
2021-12-29 21:39:56 -0300

    update

--- pages/blog.js
+++ pages/blog.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Button from '../components/Buttons/Button';
-import Container from '../components/container';
+import Container from '../components/Container';
 import Layout from '../components/Layout/Layout';
 
 export default function blog() {
